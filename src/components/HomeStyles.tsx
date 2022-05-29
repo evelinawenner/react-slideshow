2f1749c7fe33e2d9e9706f08fbf6dd10ef6d1faa
Evelina Wenner <evelina.nilssonvenner@medieinstitutet.se>
2022-05-29 20:36:33 +0200

    slider logic

---
+++ src/components/HomeStyles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeStyles = styled.div`
+  .image {
+    position: relative;
+    z-index: 1;
+    width: 1000px;
+  }
+`;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 
 export const Slider = () => {
   const [images, setImages] = useState([]);
+  const [currImage, setCurrImage] = useState(0);
+  const length = images.length;
 
   const getImages = async () => {
     let clientId = "eUzQm28A457kQ1rrVWI4DSQxLZZ3edeYnGpr8DKVOz0";
@@ -19,8 +22,31 @@ export const Slider = () => {
     getImages();
   }, []);
 
-  const imageList = images.map((image: any) => {
-    return <img src={image?.urls.small} />;
+  const nextImage = () => {
+    setCurrImage(currImage === length - 1 ? 0 : currImage + 1);
+  };
+
+  const prevImage = () => {
+    setCurrImage(currImage === 0 ? length - 1 : currImage - 1);
+  };
+
+  const imageList = images.map((image: any, index) => {
+    return (
+      <div
+        key={index}
+        className={index === currImage ? "active-slide" : "slide"}
+      >
+        {index === currImage && (
+          <img src={image?.urls.small} alt="" className="image" />
+        )}
+      </div>
+    );
   });
-  return <div>{imageList}</div>;
+  return (
+    <section>
+      <MdArrowBackIos className="prev-arrow" onClick={prevImage} />
+      <MdArrowForwardIos className="next-arrow" onClick={nextImage} />
+      {imageList}
+    </section>
+  );
 };
